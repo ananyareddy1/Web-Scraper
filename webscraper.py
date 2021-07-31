b494d753cf1ea2e775e22290fe8f61c5ac694817
ananyareddy1 <87062097+ananyareddy1@users.noreply.github.com>
2021-07-31 14:56:36 +0530

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -22,7 +22,6 @@ text1 = soup.get_text("p")
 
 
 #Print title and section headings
-
 for heading in soup.find_all(["h1"]):
     print('Article title: '+ heading.text.replace("[edit]", "").strip())
 
@@ -38,17 +37,9 @@ for heading in soup.find_all(["h3"]):
 print("\n")
 
 
-
-words = re.findall('\w+', text1)
-
-#Getting stopwords using nltk
-
-stop_words = stopwords.words('english')
-stopwords_dict = Counter(stop_words)
-morestopwords_dict = ['the', 'and', 'to']
-
 #Getting most frequent words and their count
 
+words = re.findall('\w+', text1)
 print('Most frequently occurring words in the article: ')
 frequent_words = Counter(words).most_common(10)
 print(frequent_words)
